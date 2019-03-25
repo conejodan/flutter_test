d2d2be66d22949f341c6619e41eef7da8030a0f6
Daniel Sanchez <daniel.sanches@apscreativas.com>
2019-03-25 09:45:05 -0600

    Agregando background a login y diseño de campos

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -18,48 +18,68 @@ class _AuthPageState extends State<AuthPage> {
         title: Text("Login"),
       ),
       body: Container(
-        margin: EdgeInsets.all(10.0),
-        child: ListView(
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(labelText: "E-mail"),
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (String value) {
-                setState(() {
-                  _emailValue = value;
-                });
-              },
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.4), BlendMode.dstATop),
+              image: AssetImage("assets/background.jpg"),
             ),
-            TextField(
-              decoration: InputDecoration(labelText: "Password"),
-              obscureText: true,
-              onChanged: (String value) {
-                setState(() {
-                  _passwordValue = value;
-                });
-              },
-            ),
-            SwitchListTile(
-              title: Text("Remember"),
-              value: _rememberValue,
-              onChanged: (bool value) {
-                setState(() {
-                _rememberValue = value;  
-                });
-              },
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            RaisedButton(
-              child: Text("Login"),
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, "/products");
-              },
-            )
-          ],
-        ),
-      ),
+          ),
+          padding: EdgeInsets.all(10.0),
+          child: Center(
+            child: SingleChildScrollView(
+                padding: EdgeInsets.only(bottom: 60.0),
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: "E-mail",
+                        filled: true,
+                        fillColor: Colors.white,
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                      onChanged: (String value) {
+                        setState(() {
+                          _emailValue = value;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 10.0,),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        filled: true,
+                        fillColor: Colors.white,
+                      ),
+                      obscureText: true,
+                      onChanged: (String value) {
+                        setState(() {
+                          _passwordValue = value;
+                        });
+                      },
+                    ),
+                    SwitchListTile(
+                      title: Text("Remember"),
+                      value: _rememberValue,
+                      onChanged: (bool value) {
+                        setState(() {
+                          _rememberValue = value;
+                        });
+                      },
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    RaisedButton(
+                      child: Text("Login"),
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(context, "/products");
+                      },
+                    )
+                  ],
+                )),
+          )),
     );
   }
 }
